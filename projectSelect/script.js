cee7749d49c653d9fdc7d19e1c8aa66965a068e6
jaymac00 <jmm0191@auburn.edu>
2021-11-10 20:26:46 -0600

    general config

--- projectSelect/script.js
+++ projectSelect/script.js
@@ -1,1 +1,4 @@
-function writer() {}
+const imgs = ["32", "50"];
+function randomImg() {
+	document.getElementById("random").innerHTML = "<img src=\"" + imgs[Math.floor(Math.random() * imgs.length)] + "px_filler.png\">";
+}
